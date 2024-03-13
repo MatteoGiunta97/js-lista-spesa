db01c6ceb4a3a5039afa0cf44b0a764fcfc2a3b0
MatteoGiunta97 <matteogiunta@outlook.it>
2024-03-13 14:35:05 +0100

    init

---
+++ lista-for/js/script.js
@@ -0,0 +1,14 @@
+/*
+Consegna:
+Data una lista della spesa:
+const list = [
+    'Latte',
+    'Cacao',
+    'Insalata',
+    'Carote',
+    'Pomodori',
+    'Piatti pronti'
+];
+Stampare sulla pagina (anche brutalmente, basta che si vedano) 
+gli elementi della lista individualmente.
+*/
