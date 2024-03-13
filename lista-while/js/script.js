5a5ea7bd908b9580e77ed8f5438ed43cf4220c67
MatteoGiunta97 <matteogiunta@outlook.it>
2024-03-13 15:33:00 +0100

    feat: add while loop

--- lista-while/js/script.js
+++ lista-while/js/script.js
@@ -12,3 +12,36 @@ const list = [
 Stampare sulla pagina (anche brutalmente, basta che si vedano) 
 gli elementi della lista individualmente.
 */
+
+
+
+// Dichiaro l'array lista
+const list = [
+    'Latte',
+    'Cacao',
+    'Insalata',
+    'Carote',
+    'Pomodori',
+    'Piatti pronti'
+];
+
+// Seleziono la lista dal DOM
+const groceryList = document.querySelector('#grocery-list');
+
+// Dichiaro la variabile contatore
+let index = 0;
+
+// Leggo gli elementi della lista
+while (index < list.length) {
+
+    // Inserisco ogni elemento in un list item
+    let listItem = `<li>${list[index]}</li>`;
+
+    // Inserisco il list item all'interno della unordered list del DOM
+    groceryList.innerHTML += listItem;
+
+    index++;
+    
+}
+
+
